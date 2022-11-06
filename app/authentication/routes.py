b85b09db3ce25f9c1e3eab5305607888e7efe1ce
andrew kraus <andrewkraus521@gmail.com>
2022-11-05 20:44:42 -0600

    cannot import flask proporly and the utilities ask for help with the __init__.py and the auth.site/auth.auth routes

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -2,36 +2,42 @@ from forms import UserLoginForm
 from models import User, db, check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
-from flask_login import login_user, logout_user, Login_manager, current_user, login_required
+# imports for flask login 
+from flask_login import login_user, logout_user, LoginManager, current_user, login_required
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
-
-@auth.route('/signup', methods=['GET', 'POST'])
+@auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    form = UserLoginForm
-
+    form = UserLoginForm()
 
     try:
         if request.method == 'POST' and form.validate_on_submit():
             email = form.email.data
             password = form.password.data
-            print(email,password)
+            print(email, password)
 
-            user = User(email, password=password)
+            user = User(email, password = password)
+
             db.session.add(user)
             db.session.commit()
 
-            flash(f'You have successfully created a user account {email}','User-created')
+
+
+            flash(f'You have successfully created a user account {email}', 'User-created')
             return redirect(url_for('site.home'))
     except:
-        raise Exception('Invalid form data please check your form')
-    return render_template('sign_up.html',form = form)
+        raise Exception('Invalid form data: Please check your form')
+    return render_template('sign_up.html', form=form)
 
-@auth.route('/signin',methods = ['GET','POST'])
+
+
+
+
+@auth.route('/signin', methods = ['GET', 'POST'])
 def signin():
-    form = UserLoginForm
-
+    form = UserLoginForm()
+    
     try:
         if request.method == 'POST' and form.validate_on_submit():
             email = form.email.data
@@ -39,15 +45,16 @@ def signin():
             print(email,password)
 
             logged_user = User.query.filter(User.email == email).first()
-            if logged_user and check_password_hash(logged_user.password,password):
+            if logged_user and check_password_hash(logged_user.password, password):
                 login_user(logged_user)
-                flash('congrats you made it in da club' 'auth-success')
+                flash('You were successful in your initiation. Congratulations, and welcome to the Jedi Knights', 'auth-sucess')
                 return redirect(url_for('site.profile'))
             else:
-                flash('kick rocks my fellow!','auth-failed')
+                flash('You have failed in your attempt to access this content.', 'auth-failed')
+                return redirect(url_for('auth.signin'))
     except:
-        raise Exception('thats not valid form data dude')
-    return render_template('sign_in.html',form = form)
+        raise Exception('Invalid Form Data: Please Check your Form')
+    return render_template('sign_in.html', form=form)
 
 @auth.route('/logout')
 def logout():
