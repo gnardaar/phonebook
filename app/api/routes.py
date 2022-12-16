b266fb95daa54387ef3d19c16ea52ab5c16559f1
andrew kraus <andrewkraus521@gmail.com>
2022-12-16 11:30:19 -0700

    adding aditional routes to the api, cannot get app running

--- app/api/routes.py
+++ app/api/routes.py
@@ -19,13 +19,48 @@ def create_contact(current_user_token):
 
     print(f'BIG tester: {current_user_token.token}')
 
-    #need get request and delete and update(  'PUT'  ) method
-
-
-
     contact = Contact(name,email,phone_number,address,user_token=user_token)
     db.session.add(contact)
     db.session.commit()
 
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+
+@api.route('/contacts', methods = ['GET'])
+@token_required
+def get_contact(current_user_token):
+    a_user = current_user_token.token
+    contacts = Contact.query.filter_by(user_token = a_user).all()
+    response = contacts_schema.dump(contacts)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['GET'])
+@token_required
+def get_single_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['POST','PUT'])
+@token_required
+def update_contact(current_user_token,id):
+    contact = Contact.query.get(id) 
+    contact.name = request.json['name']
+    contact.email = request.json['email']
+    contact.phone_number = request.json['phone_number']
+    contact.address = request.json['address']
+    contact.user_token = current_user_token.token
+
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['DELETE'])
+@token_required
+def delete_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
     response = contact_schema.dump(contact)
     return jsonify(response)
